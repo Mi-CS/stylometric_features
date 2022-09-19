2f5e005db4370607715a6154386f1ac58daa6473
Mi-CS <miguel.carreterosahuquillo@gmail.com>
2022-09-19 13:11:08 +0200

    multi-author graph added

--- src/multiauth_graph.py
+++ src/multiauth_graph.py
@@ -4,17 +4,61 @@ from collections import Counter
 
 from distances import mod_hausdorff_dist
 
-def get_frag_dist(pc_list: list) -> np.ndarray:
-    """
-    Take a list of point clouds and compute
-    the Modified Hausdorff distance between 
-    them. Each entry of the list represents
-    a document and contains the point clouds
-    of the fragments for that document. 
 
-    By default, the distance between any two 
-    fragments from the same text is -1. 
-    """ 
+def doc_adjacency_matrix(pc_list: list,
+                        method: Literal["closest", "threshold"],
+                        n_fragments: int = 4,
+                        threshold: float = 0.1, 
+                        min_fragments: int = 1) -> np.ndarray: 
+
+    """
+    Take a list of point clouds and compute the document adjacency
+    matrix according to the specified method. It first compute the distances
+    between any pair of fragments contained in the point clouds. By default, the distance
+    between any pair of fragment belonging to the same cloud is infinite. 
+    If 'method=closest', for each fragment keeps the closest 'n_fragments'. 
+    If 'method=threshold', distances are normalized to 0-1, and
+    keep all fragments below 'threshold'. 
+    It then links each pair of documents if at least 'min_fragments' number
+    of fragments are linked. 
+
+    Parameters: 
+        pc_list (list or pandas Series): List of point clouds. Each entry of the list represents
+            a document and contains the point clouds of the fragments for that document. 
+        method (str) ('closest' or 'threshold'): Method employed to compute the 
+            document adjacency matrix
+        n_fragments (int) (Optional, def: 4): Only used when 'method=closest'. Number of 
+            closest fragments to each single fragments to take into account. 
+        threshold (float) (Optional, def: 0.1): Only used when 'method=threshold'. 
+            Maximum distance to link fragments. 
+        min_fragments (int) (Optional, def: 1): Minimum number of linked fragments
+            needed to link two documents. By default 1, i.e., consider all linked 
+            fragments. 
+
+    Returns: 
+        adjacency_matrix (numpy.ndarray): Adjacency matrix of documents
+    """
+    assert n_fragments > 0, "n_fragments must be an integer greater than zero."
+
+    # Compute fragment list
+    frag_list = _get_frag_dist(pc_list)
+
+    # Get vector indicating to which document each
+    # fragment belongs to
+    doc_idx = [[i]*len(pc) for i, pc in enumerate(pc_list)]
+    doc_idx = [x for y in doc_idx for x in y]
+
+    adj_matrix = _get_adjacency(dist_matrix = frag_list,
+                  doc_idx = doc_idx,
+                  method = method,
+                  n_fragments = n_fragments,
+                  threshold = threshold, 
+                  min_fragments = min_fragments)
+
+    return adj_matrix
+
+
+def _get_frag_dist(pc_list: list) -> np.ndarray:
     # Create a flatten list of indices indicating
     # to which document each fragment belongs to
     doc_lengths = [len(doc) for doc in pc_list]
@@ -41,49 +85,23 @@ def get_frag_dist(pc_list: list) -> np.ndarray:
     return dist_matrix
 
 
-def get_adjacency(dist_matrix: np.ndarray,
+def _get_adjacency(dist_matrix: np.ndarray,
                   doc_idx: np.ndarray,
                   method: Literal["closest", "threshold"],
-                  n_fragments: int = 4,
-                  threshold: float = 0.1, 
-                  min_fragments: int = 1):
-
-    """
-    Compute the document adjacency matrix according to the specified method from the
-    fragment distance matrix. If 'method=closest', for each fragment keeps the 
-    closest 'n_fragments'. If 'method=threshold', distances are normalized to 0-1, and
-    keep all fragments below 'threshold'. Fragments belonging to the same document are
-    considered to be infinitely separated. It then links each pair of documents if at 
-    least 'min_fragments' are linked.    
-
-    Parameters: 
-        dist_matrix (numpy.ndarray): Distance matrix of fragments
-        doc_idx (numpy.ndarray): Array of length number of fragments. Each entry 
-            indicates to which document each fragment belongs to. 
-        method (str) ('closest' or 'threshold'): Method employed to compute the 
-            document adjacency matrix
-        n_fragments (int) (Optional, def: 4): Only used when 'method=closest'. Number of 
-            closest fragments to each single fragments to take into account. 
-        threshold (float) (Optional, def: 0.1): Only used when 'method=threshold'. 
-            Maximum distance to link fragments. 
-        min_fragments (int) (Optional, def: 1): Minimum number of linked fragments
-            needed to link two documents. By default 1, i.e., consider all linked 
-            fragments. 
-
-    Returns: 
-        adjacency_matrix (numpy.ndarray): Adjacency matrix of documents
-    """
+                  n_fragments: int,
+                  threshold: float, 
+                  min_fragments: int) -> np.ndarray:
 
     dist_matrix = np.where(dist_matrix == -1, np.inf, dist_matrix)
 
     assert method in ["closest", "threshold"], "method can only be 'closest' or 'threshold'"
 
     if method == "closest": 
-        return _get_adjacency_closest(dist_matrix, doc_idx, n_fragments)
+        return _get_adjacency_closest(dist_matrix, doc_idx, n_fragments, min_fragments)
     if method == "threshold": 
-        return _get_adjacency_threshold(dist_matrix, doc_idx, threshold)
+        return _get_adjacency_threshold(dist_matrix, doc_idx, threshold, min_fragments)
 
-def _get_adjacency_closest(dist_matrix, doc_idx, n_fragments): 
+def _get_adjacency_closest(dist_matrix, doc_idx, n_fragments, min_fragments): 
     closest_frag_idx = dist_matrix.argsort(axis=1)[:, :n_fragments]
 
     def get_doc_idx(frag_idx):
@@ -92,9 +110,9 @@ def _get_adjacency_closest(dist_matrix, doc_idx, n_fragments):
 
     closest_doc_idx = get_doc_idx(closest_frag_idx)
     
-    return _build_adjacency_matrix_closest(closest_doc_idx, doc_idx)
+    return _build_adjacency_matrix_closest(closest_doc_idx, doc_idx, min_fragments)
 
-def _get_adjacency_threshold(dist_matrix, doc_idx, threshold):
+def _get_adjacency_threshold(dist_matrix, doc_idx, threshold, min_fragments):
 
     # Normalize dist_matrix
     min_dist = dist_matrix.min()
@@ -119,13 +137,13 @@ def _get_adjacency_threshold(dist_matrix, doc_idx, threshold):
         for idx in closest_docs_idx: 
             if idx == -1:
                 continue
+            if closest_docs_idx[idx] < min_fragments:
+                continue
             adj_matrix[i, idx] += closest_docs_idx[idx]
 
     return adj_matrix
 
-
-
-def _build_adjacency_matrix_closest(closest_doc_idx, doc_idx): 
+def _build_adjacency_matrix_closest(closest_doc_idx, doc_idx, min_fragments): 
     # Get indices where to cut
     counts = Counter(doc_idx)
     cut_idx = np.cumsum([counts[i] for i in range(len(counts))])[:-1]
@@ -137,6 +155,8 @@ def _build_adjacency_matrix_closest(closest_doc_idx, doc_idx):
         closest_docs_idx = Counter(doc.flatten())        
 
         for idx in closest_docs_idx: 
+            if closest_docs_idx[idx] < min_fragments: 
+                continue
             adj_matrix[i, idx] += closest_docs_idx[idx]
     
     return adj_matrix
